69a9c77c381c5cb939d179ebccc4f8ef27f5d7ee
Chad <chad.doebelin@gmail.com>
2025-09-01 16:04:24 -0500

    first lets make sound

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Build psgtest.exe with Microsoft C 6.0
+rem Assumes your C600 BIN is on PATH (as in your AUTOEXEC.BAT)
+cl /O /AS /Fepsgtest.exe src\psgtest.c
+link /NOI /TINY src\psgtest.obj, psgtest.exe,,;
